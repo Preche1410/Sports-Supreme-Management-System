4d72bc9a2d45ab04e320dadf87b9547f3108226a
Preche Florian <amourdreche2@gmail.com>
2020-09-21 15:39:53 +0300

    first commit

---
+++ src/main/java/precheflorian/sports-supreme-management-system/SportsSupremeManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package precheflorian.footballmanagementsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FootballmanagementsystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FootballmanagementsystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/precheflorian/footballmanagementsystem/FootballmanagementsystemApplicationTests.java
@@ -0,0 +1,13 @@
+package precheflorian.footballmanagementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FootballmanagementsystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
